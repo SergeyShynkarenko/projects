bfc0c78cdf03afe4a49fc1671f1996a127b99e89
SergeyShynkarenko <serg.shunkarenko@gmail.com>
2021-02-19 19:43:01 +0300

    cards

--- app/js/app.js
+++ app/js/app.js
@@ -1,5 +1,14 @@
-document.addEventListener("DOMContentLoaded", function() {
+const panels = document.querySelectorAll('.panel')
 
-	// Custom JS
+panels.forEach(panel => {
+	panel.addEventListener('click', () => {
+		removeActiveClasses()
+		panel.classList.add('active')
+	})
+})
 
-});
+function removeActiveClasses() {
+	panels.forEach(panel => {
+		panel.classList.remove('active')
+	})
+}
